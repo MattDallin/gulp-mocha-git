436772c4411c6bae790cccea01225404e4aca223
matt <matthew.kastendieck@glazrsoftware.com>
2015-06-24 07:57:13 -0600

    gulp commit - Wed Jun 24 2015 07:57:13 GMT-0600 (MDT)

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@
     var gitUsername = config.gitUsername || false;
     var gitPassword = config.gitPassword || false;
     var gitOrigin = (!!gitUsername && !!gitPassword) ? 'https://' + gitUsername + ':' + gitPassword + '@' + gitRepoUrl : 'https://' + gitRepoUrl;
-    console.log(!!gitUsername);
+    console.log(gitUsername);
 
     git.push(gitOrigin, gitBranch, {}, function (err) {
       if (err) throw err;
